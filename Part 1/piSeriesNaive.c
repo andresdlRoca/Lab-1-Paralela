c69d6e6bebfb7b3ed23d6101ceb73c0f839515e8
Paola Contreras <paolacontreras231@gmail.com>
2023-08-19 00:06:24 -0600

    fix

--- Part 1/piSeriesNaive.c
+++ Part 1/piSeriesNaive.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
         thread_count = atoi(argv[2]);
     }
 
-    #pragma omp parallel for num_threads (thread_count) \ reduction(+:sum)
+    #pragma omp parallel for num_threads (thread_count) reduction(+:sum)
     for (int i = 0; i < n; i++) {
         sum += factor/(2*i+1);
         factor = -factor;
